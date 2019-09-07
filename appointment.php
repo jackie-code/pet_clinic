721e43f04bb00531556eaf25a916223275f30060
jackie-code <paradiseprofessional88@gmail.com>
2019-09-07 23:52:42 +0800

    another update with css stuff

--- appointment.php
+++ appointment.php
@@ -5,6 +5,25 @@ include "header_nav.php"
     
 <main>
 
+<div id="left">
+        LEFT COLUMN CONTENT
+    </div>
+
+    <div id="mid">
+        <!-- Make this form so that when user clicks radio button either a happy/sad face appears -->
+        <form name="formA">
+            <p>How was your experience?</p>
+            <input type="radio" id="feelh" name="answer" value="happy">
+            <label id="h" for="feelh">Great</label>
+            <input type="radio" id="feels" name="answer" value="sad">
+            <label id="s" for="feels">Not Satisfied</label>
+        </form>
+    </div>
+
+    
+
+<br>
+
     <form action="appointment.html" method="POST">
 <!-- pet name input -->
 <label for="petname">Pet's Name</label>
@@ -43,6 +62,10 @@ include "header_nav.php"
 
     </form>
     
+
+    <div id="right">
+        RIGHT COLUMN CONTENT
+    </div>
 </main>
 
 
@@ -51,9 +74,9 @@ include "header_nav.php"
 
 
     
-    <footer>
-        FOOTER
-    </footer>
+<?php
+include "footer.php";
+?>
 
     <script src="jquery-3.4.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- flashmessage.php
+++ flashmessage.php
@@ -1,38 +1,55 @@
 <?php
 include "header_nav.php"
 ?>
-    
-    <main>
-    <div id="left">
-        LEFT COLUMN CONTENT
+
+<main>
+
+    <div id="homediv">
+
+        <div id="stayl">
+            <p>
+                You don’t just share a home with your dog or cat. They are truly family to you. We understand that to our bones. And the attachment that you feel to your pet is irreplaceable. The moments you share, the laughs you have, the love you feel… it’s priceless. And when you care so much for an animal, you want to know that they are receiving the best care there is. You want to know that you are doing right by them, always. Because the love that you share means the world to you. It means the world to us, too. And we’ll help them live a longer, happier life by your side.
+            </p>
+        </div>
+
+        <div id="docs">
+            <h2>Our Staff</h2>
+
+            <h5>Dr Jamie</h5>
+            <img src="flash_images/vet1.jpg" alt="">
+            <p>
+                She was born and raised in Hong Kong, after which she moved to Switzerland and studied veterinary medicine. Dr. Gerry was accepted at the Washington State University to attend an internship in Small Animal Medicine and Surgery. After this, she gathered information for her thesis on Iguana Anesthesia at the Long Beach Animal Hospital in California. She worked at the Marathon Veterinary Clinic with Dr. Jim Greene for almost two years before purchasing the practice and becoming the new owner of the new Marathon Veterinary Hospital. Apart from her obligations at work, Dr. Gerry also donates time and materials to the Wild Bird Center run by Kelly Grinter.
+            </p>
+
+            <h5>Dr Pino</h5>
+            <img src="flash_images/vet2.jpg" alt="">
+            <p>
+                Dr. Pino grew up in Tennessee and moved to Florida in 1996 where she started working in wild bird rehabilitation in Tavernier with Laura Quinn. After her experience there she decided that to better help the birds she needed to go to Veterinary School. Dr. del Pino will always be grateful to Laura for her experience there and for being an inspiration to further her education. Dr. del Pino finished her Bachelor’s degree from the University of Florida in Wildlife Conservation and Ecology. She then attended Veterinary School in the Cayman Islands, St. Matthew’s University where she was fortunate enough to have received a vast amount of exotic animal experience. She had her clinical year at the University of Illinois and did a year internship in wildlife medicine at C.R.O.W, The Clinic for the Rehabilitation of Wildlife on Sanibel Island. After her internship, she began work at Banfield Pet Hospital and was there for 2.5 years until she received a position in Marathon with Dr. Mader. “I feel fortunate that I was able to move here and work with such wonderful people and be able to learn from everyone.”
+            </p>
+
+
+            <h5>Dr. Trisha</h5>
+            <img src="flash_images/vet3.jpg" alt="">
+            <p>Dr. Trisha Doswell grew up on the East Coast Barrier Islands of Central Florida. She fell in love with animals watching tortoises and bobcats roam the backyard, sea turtles nesting and hatching on the beaches, along with caring for a variety of her own pets. She attended University of Florida where she received her Bachelors of Science in Animal Science with a minor of Fisheries and Aquatic Sciences. Go Gators! Dr. Doswell attended St. George’s University in Grenada where she received her Doctorate of Veterinary Medicine. She completed her clinical year at Louisiana State University focusing on zoo and exotic medicine. Her interests include internal medicine, surgery, rehabilitation and dermatology.
+            </p>
+
+        </div>
+
     </div>
 
-    <div id="mid">
-        <!-- Make this form so that when user clicks radio button either a happy/sad face appears -->
-        <form name="formA">
-            <p>How was your experience?</p>
-            <input type="radio" id="feelh" name="answer" value="happy">
-            <label id="h" for="feelh">Great</label>
-            <input type="radio" id="feels" name="answer" value="sad">
-            <label id="s" for="feels">Not Satisfied</label>
-        </form>
-    </div>
+</main>
 
-    <div id="right">
-        RIGHT COLUMN CONTENT
-    </div>
-    </main>
-    
-    <footer>
-        FOOTER
-    </footer>
+<?php
+include "footer.php";
+?>
 
-    <script src="jquery-3.4.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="jquery-3.4.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <!-- Your code needs to be loaded AFTER jQuery, Bootstrap, etc. -->
-    <script src="app.js"></script>
+<script src="app.js"></script>
 
 
 </body>
+
 </html>
