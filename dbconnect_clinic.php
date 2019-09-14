2f1bc1eff5c07034d106094d63b308f59e1c61c3
jackie-code <paradiseprofessional88@gmail.com>
2019-09-15 04:23:18 +0800

    changed dbconnect

--- dbconnect_clinic.php
+++ dbconnect_clinic.php
@@ -1,10 +1,34 @@
 
 <?php
+//default for localhost values
+$host = "localhost";
+$port = "3306";
+$user = "jmartin";
+$pw = "codeschool1";
+$dbname = "jmartin_clinic";
+
+// for PBCS server
+if (gethostname() == "server.pbcs.us")
+{
 $host = "localhost";
 $port = "3306";
 $user = "jmartin";
 $password = "codeschool1";
 $database = "jmartin_clinic";
+}
+
+$connection_info = 
+"mysql" 
+.": host=" . $host
+.":" . $port
+. "; dbname=" . $dbname
+;
+ 
+$db = new PDO($connection_info, $user, $pw);
+
+
+
+
 
 
 $conn_str = "mysql: host=localhost:3306; dbname=$database";
