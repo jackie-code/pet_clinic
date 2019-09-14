8559eb9dac1326e18a09cdb382a34f2983f48130
jackie-code <paradiseprofessional88@gmail.com>
2019-09-15 05:03:22 +0800

    corrected images

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 
 <footer>
-<a href="mailto:name@sampleemail.com"><img src="flash_images/phone.png" alt="email"></a>
-<a href="tel:555-555-5555"><img src="flash_images/mail.png" alt="phone"></a>
+<a href="tel:555-555-5555"><img src="flash_images/phone.png" alt="phone"></a>
+<a href="mailto:name@sampleemail.com"><img src="flash_images/mail.png" alt="email"></a>
 </footer>
+
